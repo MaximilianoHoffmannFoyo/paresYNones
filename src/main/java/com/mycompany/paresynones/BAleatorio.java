0201a5a967c1b9ff39e3967cdd091afd44873df4
Maxi <mhof0902@g.educaand.es>
2021-11-02 13:34:07 +0100

    Añadidos comentarios y bucle final para menu de repetir el juego

--- src/main/java/com/mycompany/paresynones/BAleatorio.java
+++ src/main/java/com/mycompany/paresynones/BAleatorio.java
@@ -25,39 +25,56 @@ public class BAleatorio {
                 if(!(ganador == 1 || ganador == 2)){
                     System.out.println("dato erroneo");
                 }
-            } while(ganador != 1 ? ganador != 2 : 1==2) ;
+            } while(ganador != 1 ? ganador != 2 : 1==2);
+                                    // el 1==2 es solo un booleano falso de relleno
         
-            do{
+            do{//bucle para que no se metan mas de 5 dedos
                 System.out.println("");
                 System.out.println("Introduce número de dedos para jugar: ");
                 dedosJugador = entrada.nextInt();
+                //if para que se siga el bucle si no esta el numero en el rango indicado
+                // el 2 es un dato cualquiera para igualar la variable dedosCorrectos en 
+                //el while y que asi se repita el codigo si es erroneo
                 if(dedosJugador>=1 && dedosJugador<=5){
                     dedosCorrectos = 2;
-                } 
+                }
+                //if para decir que el dato introducido es erroneo 
+                if(!(dedosJugador>=1 && dedosJugador<=5)){
+                    System.out.println("dato erroneo");
+                }
             } while(!(dedosCorrectos == 2));
             
+            //uso de Scanner para sacar dedos del 1 a 5 de forma aleatoria  
             dedosCPU = aleatorio.nextInt(5)+1;
             System.out.println("La cpu saca: "+dedosCPU);
-        
+            
+            //operaciones para calcular el ganador 
             totalDedos = dedosJugador+dedosCPU;
-            if(totalDedos%2 == 0){
+            if(totalDedos%2 == 0){//si el total de dedos es par 
                 if(ganador == 1){
                     System.out.println("Ganas");
                 }else{
                     System.out.println("Pierdes");
                 }
-            }else{
+            }else{//si el total de dedos es inpar
                 if(ganador == 1){
                     System.out.println("Pierdes");
                 }else{
                     System.out.println("Ganas");
                 }
             }
-            System.out.println("");
-            System.out.println("¿Quieres seguir jugando?");
-            System.out.println("1='SI' o 2='NO'");
-            seguir = entrada.nextInt(); 
-        } while(seguir==1);
+            do{//bucle para que se seleccione solo 1 o 2 y se repita si no
+                System.out.println("");
+                System.out.println("¿Quieres seguir jugando?");
+                System.out.println("1='SI' o 2='NO'");
+                seguir = entrada.nextInt(); 
+                if(!(ganador == 1 || ganador == 2)){
+                    System.out.println("dato erroneo");
+                }
+            } while(ganador != 1 ? ganador != 2 : 1==2);
+                                    // el 1==2 es solo un booleano falso de relleno
+                                    
+        } while(seguir==1);//final del bucle para repetir todo el juego
         
     }
 }
