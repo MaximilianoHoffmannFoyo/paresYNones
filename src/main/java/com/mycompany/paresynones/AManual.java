6143ea22f074c36af5db37401ca5fa48e8e66af7
Maxi <mhof0902@g.educaand.es>
2021-11-02 12:34:12 +0100

    juego hecho

--- src/main/java/com/mycompany/paresynones/AManual.java
+++ src/main/java/com/mycompany/paresynones/AManual.java
@@ -1,9 +1,38 @@
 package com.mycompany.paresynones;
 
+import java.util.Scanner;
+
 public class AManual {
     
     public static void main(String[] args) {
+        //declaramos e inicializamos las variables y objetos necesarios
+        Scanner entrada = new Scanner(System.in);               
         
+        System.out.println("Que el jugador 1 elija: ");
+        System.out.println("1='pares' o 2='nones'");
+        int ganador = entrada.nextInt();
+        
+        System.out.println("Jugador 1");
+        System.out.println("Introduce número de dedos");
+        int dedosJ1 = entrada.nextInt();
+        
+        System.out.println("Jugador 2");
+        System.out.println("Introduce número de dedos");
+        int dedosJ2 = entrada.nextInt();
+        
+        int totalDedos = dedosJ1+dedosJ2;
+        if(totalDedos%2 == 0){
+            if(ganador == 1){
+                System.out.println("Gana el jugador 1");
+            }else{
+                System.out.println("Gana el jugador 2");
+            }
+        }else{
+            if(ganador == 1){
+                System.out.println("Gana el jugador 2");
+            }else{
+                System.out.println("Gana el jugador 1");
+            }
+        }
     }
-    
 }
